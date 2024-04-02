a6d1470f59746a3ce36cb7bce4506d6fcecd76c5
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-04-02 21:28:10 +0000

    Dropped favorites id and added station id and user id to notification times table

--- app/models/notification_time.rb
+++ app/models/notification_time.rb
@@ -2,21 +2,25 @@
 #
 # Table name: notification_times
 #
-#  id           :integer          not null, primary key
-#  recurrence   :string
-#  time         :time
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  favorites_id :integer          not null
+#  id         :integer          not null, primary key
+#  recurrence :string
+#  time       :time
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  station_id :integer          not null
+#  user_id    :integer          not null
 #
 # Indexes
 #
-#  index_notification_times_on_favorites_id  (favorites_id)
+#  index_notification_times_on_station_id  (station_id)
+#  index_notification_times_on_user_id     (user_id)
 #
 # Foreign Keys
 #
-#  favorites_id  (favorites_id => favorites.id)
+#  station_id  (station_id => stations.id)
+#  user_id     (user_id => users.id)
 #
 class NotificationTime < ApplicationRecord
   belongs_to :user
+  
 end
---
+++ db/migrate/20240402212341_add_user_ref_to_notification_times.rb
@@ -0,0 +1,5 @@
+class AddUserRefToNotificationTimes < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :notification_times, :user, null: false, foreign_key: true
+  end
+end
