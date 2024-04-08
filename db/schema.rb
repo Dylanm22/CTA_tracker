18e8ce4b944b4fb8b8c1cd2111e61b7bc0bd0748
Dylanm22 <dylanmenard222@gmail.com>
2024-04-07 19:00:25 -0500

    took username column out of users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_03_161232) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_07_235115) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "station_id", null: false
@@ -44,12 +44,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_03_161232) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "favorites", "stations"
